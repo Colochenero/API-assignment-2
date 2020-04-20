7ec0a9c253f7f630a82055f049bb7dbc000f3354
Colochenero <mostafa6520@gmail.com>
2020-04-20 23:07:19 +0200

    CACHING with Its test CASES

--- app.js
+++ app.js
@@ -1,34 +1,75 @@
 const axios = require('axios');
+var fs = require("fs");
+const path = require('path');
 const http = require('http');
 const url = require('url');
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather?q=';
  const API_KEY = 'b4642b3321c9e21a4e1efc5ffe1b296c';
  var ENTIRE_API_URL;
+ var Full_Path; 
 const x = 459.67;
 const y = 273.15;
-
-
 http.createServer(function(req,res){
     const cityName= req.url.replace('/','');
- ENTIRE_API_URL = `${API_URL}${cityName},&appid=${API_KEY}`
-getResponse(res);
-
+ ENTIRE_API_URL = `${API_URL}${cityName},&appid=${API_KEY}`;
+ Full_Path = __dirname+"\\Caching Requests\\" + cityName + ".txt";
+const directoryPath = path.join(__dirname, 'Caching Requests');
+Cached = CheckCache(directoryPath,cityName);
+if(Cached)
+{
+GetCahedData(res,Full_Path);
+}
+else
+{
+ getResponse(res,Full_Path);
+}
 }).listen(8000);
+
+function CheckCache(Full_Path,cityName)
+{
+    exist = false
+    filename = cityName + ".txt";
+    //check if the City is already exists
+    fs.readdirSync(Full_Path).forEach(file => {
+        if (file == filename) {
+            exist = true;
+        }
+    });
+return exist;
+}
+function GetCahedData(res,Full_Path)
+{
+    Str_Response = fs.readFileSync(Full_Path);
+    json_Response = JSON.parse(Str_Response);
+    const message = constructMessageData(json_Response);
+       console.log(message);
+       res.write(message);
+       res.end();
+}
+function writeNewCacheData(response,Full_Path)
+{
+    var fs = require('fs');
+   response;
+   str = JSON.stringify(response.data);
+   fs.writeFile(Full_Path, str, function (err) {
+    if (err) console.log(err);
+     });
+}
 async function SendREQ()
 {
-   var result = await axios.get(ENTIRE_API_URL);  
+   var result = await axios.get(ENTIRE_API_URL);
+   //const directoryPath = path.join(dirname, 'Caching Requests');
    return result;
 }
-const getResponse = (res)=>{
+const getResponse = (res,Full_Path)=>{
    
-    return SendREQ().then(response => {
-           // Building the final message.
+    return SendREQ().then(response =>{
+          writeNewCacheData(response,Full_Path);
            const message = constructMessage(response);
            console.log(message);
            res.write(message);
            res.end();
-           return message;
-    
+           return message; 
        })
    
        .catch(error => console.log(''));    
@@ -50,6 +91,32 @@ const getResponse = (res)=>{
         return 1;
     }
    
+function getkelvinData(response)
+{
+    return response.main.temp;
+}
+function getcitynameData(response)
+{
+    return response.name;
+}
+function getcountrynameData(response)
+{
+    return response.sys.country;
+}
+function constructMessageData(response)
+{
+    const kelvinTemperature = getkelvinData(response);
+        const cityName = getcitynameData(response);
+        const countryName = getcountrynameData(response);
+        // Making K to F and K to C conversions.
+        const fahrenheitTemperature = cfahrenheitTemperature(kelvinTemperature);
+        const celciusTemperature = changeCelciusTemperature(kelvinTemperature);
+ const messageX = `Right now, in \
+    ${cityName}, ${countryName} the current temperature is \
+    ${fahrenheitTemperature} deg F or \
+    ${celciusTemperature} deg C.`.replace(/\s+/g, ' ');
+    return messageX;
+}
 function getkelvin(response)
 {
     return response.data.main.temp;
@@ -76,7 +143,6 @@ function constructMessage(response)
     ${celciusTemperature} deg C.`.replace(/\s+/g, ' ');
     return messageX;
 }
-
 exports.getResponse = getResponse;
 module.exports={cfahrenheitTemperature,
     changeCelciusTemperature,
@@ -85,6 +151,11 @@ constructMessage,
 getcityname,
 getcountryname,
 getResponse,
-SendREQ
+SendREQ,
+GetCahedData,
+CheckCache,
+writeNewCacheData,
+constructMessageData,
+getcitynameData,
+getcountrynameData,
 };
-
--- test.js
+++ test.js
@@ -1,22 +1,30 @@
 const assert = require('assert');
 const app = require('./app.js');
+http = require('http');
 
 const response= {"data":{"coord":{"lon":31.25,"lat":30.06},"weather":[{"id":701,"main":"Mist","description":"mist","icon":"50n"}],"base":"stations","main":{"temp":290,"feels_like":290.09,"temp_min":289.82,"temp_max":290.15,"pressure":1018,"humidity":88},"visibility":3000,"wind":{"speed":2.1,"deg":80},"clouds":{"all":40},"dt":1584225345,"sys":{"type":1,"id":2514,"country":"EG","sunrise":1584245080,"sunset":1584288188},"timezone":7200,"id":360630,"name":"Cairo","cod":200}}
+const responseData= {"coord":{"lon":31.25,"lat":30.06},"weather":[{"id":701,"main":"Mist","description":"mist","icon":"50n"}],"base":"stations","main":{"temp":290,"feels_like":290.09,"temp_min":289.82,"temp_max":290.15,"pressure":1018,"humidity":88},"visibility":3000,"wind":{"speed":2.1,"deg":80},"clouds":{"all":40},"dt":1584225345,"sys":{"type":1,"id":2514,"country":"EG","sunrise":1584245080,"sunset":1584288188},"timezone":7200,"id":360630,"name":"Cairo","cod":200}
 
     req = {
         url: '/CAIRO',
     }
 
-   const res = {
-        written: '',
-        write: function(str) {
-            written += str;
-        }
-    }
+    res = {
+        write: function (message) {
+            return true;
+        },
+        end: function () {
+            return true;
+        },
+        statusCode :"200",
+      }
+      const Full_Path = __dirname+"\\Caching Requests\\" + "CAIRO" + ".txt";
+      const direPath=__dirname+"\\Caching Requests\\";
+
 //test unit 1
 it('correctly yhe change from KELVIn to fahrenheitTemperature for CAIRO' , ()=>
 {
-    
+
     assert.equal(app.cfahrenheitTemperature(293.15),68.00);
 });
 //test unit 2
@@ -25,14 +33,13 @@ it('change KELVIN temp to CELCIUS for cairo',()=>
     assert.equal(app.changeCelciusTemperature(293.15),20.00);
 });
 
-//test unit 4
-//requires online
+
 it('to test SendREQ with Static object Requires **ONLINE support to do it uncomment the commented code when u become connected to the internet',()=>
 {
-   /* app.SendREQ().then(result=>{
+    app.SendREQ().then(result=>{
         assert.equal(result,response);
     })
-    */
+    
 })
 
 //test unit 5
@@ -45,9 +52,9 @@ it(' INTEGRAION test beteween SendREQ() & getcityname() requires ONLINE support*
 {
 
 //the next integeration test requires online
-   /*app.SendREQ().then(result=>{
+   app.SendREQ().then(result=>{
        assert.equal(app.getcityname(result),'Cairo')
-   })*/
+   })
 })
 //unit test
 it('to test function that returns the country of the static object RESponse', ()=>
@@ -58,17 +65,51 @@ it('to test function that returns the country of the static object RESponse', ()
 it('INTEGRAION test beteween SendREQ() & getcountryname() requires ONLINE support*** to do it uncomment the commented code when u connected to the internet', ()=>
 {
     //the next integeration test requires online
-  /* app.SendREQ().then(result=>{
+   app.SendREQ().then(result=>{
        assert.equal(app.getcountryname(result),'EG')
-   })*/
+   })
 })
 //test integration 1
 it('to test to constructmessage (integration testing)',()=>
 {
+    console.log(Full_Path);
     assert.equal(app.constructMessage(response),'Right now, in Cairo, EG the current temperature is 62.33 deg F or 16.85 deg C.');
+
 }
 )
-
-
-
-
+it('to test get cached data', ()=>
+{
+    //console.log(Full_Path);
+    assert.equal(app.GetCahedData(res,Full_Path),undefined);
+})
+it('to test CheckCache',()=>
+{
+assert.equal(app.CheckCache(direPath,"CAIRO"),true);
+})
+it('to test CheckCache2',()=>
+{
+assert.equal(app.CheckCache(direPath,"mmm"),false);
+})
+it('to test WriteNecachedata',()=>
+{
+     assert.equal(app.writeNewCacheData(response,Full_Path),undefined);
+})
+it('to ConstructMessageData',()=>
+{
+assert.equal(app.constructMessageData(responseData),'Right now, in Cairo, EG the current temperature is 62.33 deg F or 16.85 deg C.');
+})
+it('to test GetCitynameData',()=>{
+    assert.equal(app.getcitynameData(responseData),'Cairo');
+})
+it('to test getResponce',()=>{
+    assert.equal(app.getResponse(res,Full_Path),true);
+})
+it('to test get countrynamedata',()=>{
+    assert.equal(app.getcountrynameData(responseData),'EG')
+})
+    it('should return 200', function (done) {
+      http.get('http://localhost:8000', function (res) {
+        assert.equal(200, res.statusCode);
+        done();
+      });
+    })
