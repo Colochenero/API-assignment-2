928b88b5db61f0b46f3479aab65288fddd5991f6
Colochenero <mostafa6520@gmail.com>
2020-04-21 22:33:18 +0200

    updating test.js

--- test.js
+++ test.js
@@ -107,9 +107,18 @@ it('to test getResponce',async ()=>{
 it('to test get countrynamedata',()=>{
     assert.equal(app.getcountrynameData(responseData),'EG')
 })
-    /*it('should return 200', ()=> {
-      http.get('http://localhost:8000', function (resu) {
-        assert.equal(200, resu.statusCode);
-      })
-    })*/
-app.se.close();
+
+describe('server', function () {
+  before(function () {
+    app.se.listen(8000);
+  });
+  it('should return 200', ()=> {
+    http.get('http://localhost:8000', function (resu) {
+      assert.equal(200, resu.statusCode);
+    })});
+  after(function () {
+    app.se.close();
+  });
+});
+
+    app.se.close();
